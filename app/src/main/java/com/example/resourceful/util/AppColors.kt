569d7d95240c3f101342e92fc636a0541df08769
Joel Dias <joeldias0204@gmail.com>
2023-11-18 19:15:37 +0530

    God is great

---
+++ app/src/main/java/com/example/resourceful/util/AppColors.kt
@@ -0,0 +1,8 @@
+package com.example.resourceful.util
+
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val titleBar = Color(0xFF9CA9F8)
+    val resourceRow = Color(0xFFC5CEF6)
+}
